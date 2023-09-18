a506810472cb5dee4443a9f1ea0159b8aad583e6
Louis Castel <louis.castel31@gmail.com>
2023-09-18 23:36:11 +0200

    add secret env retrive

--- core/sequelize.js
+++ core/sequelize.js
@@ -1,12 +1,25 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize('nfc-server', 'postgres', 'postgres', {
-	host: 'localhost',
-	dialect: 'postgres',
-	define: {
-		freezeTableName: true,
-	},
-	logging: false,
-});
+const { Sequelize } = require('sequelize');
+console.log(process.env["rds!db-f6488152-e9bc-4ff7-a4b5-aa75d124f76e"])
+const sequelize = "ok"
+// const sequelize = new Sequelize(
+// 	'nfc-server',
+// 	'postgres',
+// 	'',
+// 	{
+// 		host: 'database-nfc-server.ckrnrijfeogt.eu-west-1.rds.amazonaws.com',
+// 		dialect: 'postgres',
+// 		define: {
+// 			freezeTableName: true,
+// 		},
+//         ssl: true,
+// 		dialectOptions: {
+// 			ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+// 			},
+// 		},
+// 		logging: false,
+// 	}
+// );
 
 module.exports = sequelize;
