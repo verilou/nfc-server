04857423f39bb51cb6a2ac72b575c4270111d5c6
Louis Castel <louis.castel31@gmail.com>
2023-10-24 16:36:37 +0200

    fuck this minor changes and fixs

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ const responseTime = require('./utils/responseTime');
 const routes = require('./routes');
 const app = new Koa();
 const isProd = process.env.NODE_ENV === 'production';
-console.log(isProd, process.env.NODE_ENV)
+console.log(
+	isProd,
+	process.env.NODE_ENV,
+	isProd ? 'https://louiscastel.fr' : '*'
+);
 app.keys = ['your-session-secret'];
 app.use(
-	cors({ origin: isProd ? 'https://louiscastel.fr' : '*' })
+	cors({
+		origin: isProd ? 'https://louiscastel.fr' : 'http://localhost:3001',
+		credentials: true,
+	})
 );
+
+app.use(async (ctx, next) => {
+	await next();
+	console.log(ctx.headers.origin);
+});
 app.use(
 	session(
 		{
