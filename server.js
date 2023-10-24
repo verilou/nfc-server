d3a52aee3397c87b39e980a050fd0b12910c1f19
Louis Castel <louis.castel31@gmail.com>
2023-10-24 14:59:45 +0200

    add https

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require('./routes');
 const app = new Koa();
 const isProd = process.env.NODE_ENV === 'production';
 app.use(
-	cors({ credentials: true, origin: isProd ? 'louiscastel.fr' : '*' })
+	cors({ credentials: true, origin: isProd ? 'https://louiscastel.fr' : '*' })
 );
 app.keys = ['your-session-secret'];
 app.use(
